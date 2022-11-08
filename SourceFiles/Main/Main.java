f3854912547881798f5bb39c49b9cffe42e7d271
Andrew Thomas James <eh2601@wayne.edu>
2022-11-07 19:09:39 -0500

    changes

--- SourceFiles/Main/Main.java
+++ SourceFiles/Main/Main.java
@@ -4,15 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-<<<<<<<<< Temporary merge branch 1
-        int bruh = 1;
-        switch (bruh)
-        {
-            case 1:
-                System.out.println("hello");
-        }
-=========
         System.out.printf("Hello!");
->>>>>>>>> Temporary merge branch 2
+
     }
 }
