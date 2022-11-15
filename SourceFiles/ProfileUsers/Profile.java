811278d4ac7bbe0bafc7f101cabe866aa79efd1e
Austin Jeffery <gy3112@wayne.edu>
2022-11-15 16:38:30 -0500

    Added Profiles Vendor and Customer and Profile Factory

--- SourceFiles/ProfileUsers/Profile.java
+++ SourceFiles/ProfileUsers/Profile.java
@@ -13,6 +13,79 @@ public abstract class Profile
     String phone;
     double balance;
     double lastPaidAmount;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getLastPaidAmount() {
+        return lastPaidAmount;
+    }
+
+    public void setLastPaidAmount(double lastPaidAmount) {
+        this.lastPaidAmount = lastPaidAmount;
+    }
+
+    public Date getLastOrderDate() {
+        return lastOrderDate;
+    }
+
+    public void setLastOrderDate(Date lastOrderDate) {
+        this.lastOrderDate = lastOrderDate;
+    }
+
     Date lastOrderDate;
 
 }
--- SourceFiles/ProfileUsers/Vender.java
+++ SourceFiles/ProfileUsers/Vender.java
@@ -1,4 +1,20 @@
 package ProfileUsers;
 
+import java.util.Date;
+
 public class Vender extends Profile{
+    Vender(){
+    }
+    Date seasonalDiscountsStartDate;
+    public Vender(int userID, String fullName, String streetAddress, String city, String state, String phone, double balance, double lastPaidAmount) {
+        this.userID = userID; //auto generated max 6 character
+        this.fullName = fullName;//must be unique
+        this.streetAddress = streetAddress;//must be unique
+        this.city = city;
+        this.state = state;
+        this.phone = phone;
+        this.balance = 0;
+        this.lastPaidAmount = 0;
+    }
+
 }
