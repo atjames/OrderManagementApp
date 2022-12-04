d0944762077fcfde5677e2aac979896f515fd7c3
Andrew Thomas James <eh2601@wayne.edu>
2022-12-03 20:31:58 -0500

    Merge remote-tracking branch 'origin/AustinsBranch' into AustinsBranch
    # Conflicts:
    #	Resources/vendors.csv

--- SourceFiles/Main/Main.java
+++ SourceFiles/Main/Main.java
@@ -1,23 +1,28 @@
 package Main;
 
+import GUI.ItemMenuGUI;
 import GUI.PurchaserView;
 import ItemProfile.ItemProfile;
 import Login.LoginMenu;
 import ProfileUsers.VendorAccountArray;
 import UserClasses.UserAccountArray;
+import ItemProfile.getItems;
 
+import javax.swing.*;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main
 {
     public static ArrayList<ItemProfile> items = new ArrayList<>();
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
         VendorAccountArray.init();
-
+        items = getItems.get_items();
         new PurchaserView();
         UserAccountArray.testAccount();
         new LoginMenu();
+        JFrame ItemMenu = new ItemMenuGUI("Item Menu");
+        ItemMenu.setVisible(true);
     }
 }
