646588c352b1ee9984b690b846578bfe3199b46c
Andrew Thomas James <eh2601@wayne.edu>
2022-12-03 20:00:28 -0500

    added main

---
+++ SourceFiles/Main/Main.java
@@ -0,0 +1,16 @@
+package Main;
+
+import ItemProfile.ItemProfile;
+
+import java.util.ArrayList;
+
+public class Main
+{
+    public static ArrayList<ItemProfile> items = new ArrayList<>();
+
+    public static void main(String[] args)
+    {
+
+
+    }
+}
