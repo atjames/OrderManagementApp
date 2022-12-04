c2236fe84a3d624bfab5f702fa7615a1f0eb5b9f
minec <hc7804@wayne.edu>
2022-12-03 20:04:04 -0500

    Hopefully will merge the login material

--- SourceFiles/Main/Main.java
+++ SourceFiles/Main/Main.java
@@ -1,6 +1,8 @@
 package Main;
 
 import ItemProfile.ItemProfile;
+import Login.LoginMenu;
+import UserClasses.UserAccountArray;
 
 import java.util.ArrayList;
 
@@ -10,7 +12,7 @@ public class Main
 
     public static void main(String[] args)
     {
-
-
+        UserAccountArray.testAccount();
+        new LoginMenu();
     }
 }
