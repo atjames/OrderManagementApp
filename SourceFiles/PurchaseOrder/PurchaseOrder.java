a903292a2e39126d6c5daac0cbc45dc98580d094
Andrew Thomas James <eh2601@wayne.edu>
2022-12-07 14:20:25 -0500

    created PurchaseOrder

--- SourceFiles/PurchaseOrder/PurchaseOrder.java
+++ SourceFiles/PurchaseOrder/PurchaseOrder.java
@@ -1,5 +1,29 @@
 package PurchaseOrder;
 
+import ItemProfile.ItemProfile;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 public class PurchaseOrder
 {
+    public static final String PURCHASE_ORDERS_CSV = "Resources/purchaseorders.csv";
+    public static final String PURCHASE_ORDER_CSV_HEADER = "itemID,purchaseOrders,vendorID,balance,needByDate";
+
+    String purchaseOrderID;
+    double totalCost;
+    Date needByDate;
+    ArrayList<ItemProfile> purchasedItems = new ArrayList<>();
+    void setPurchaseOrderID(String purchaseOrderID){this.purchaseOrderID = purchaseOrderID;}
+    void setTotalCost(double totalCost){this.totalCost = totalCost;}
+    void setNeedByDate(Date needByDate){this.needByDate = needByDate;}
+    void addPurchaseItems(ItemProfile item){purchasedItems.add(item);}
+
+    String getPurchaseOrderID(){return purchaseOrderID;}
+    double getTotalCost(){return totalCost;}
+    Date getNeedByDate(){return needByDate;}
+
+    ArrayList getPurchasedItems(){return purchasedItems;}
+
+
 }
