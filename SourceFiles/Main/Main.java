3c3466ade711b72f8f6b707da626a015a48703f9
Andrew Thomas James <eh2601@wayne.edu>
2022-12-09 14:55:01 -0500

    added purchaserorder menu button

--- SourceFiles/Main/Main.java
+++ SourceFiles/Main/Main.java
@@ -34,24 +34,7 @@ public class Main
         VendorAccountArray.init();
         items = getItems.get_items();
         PurchaseOrders = getPurchaseOrders.get_orders();
-        /*
-        PurchaseOrder order = new PurchaseOrder();
-        PurchaseOrder order2 = new PurchaseOrder();
-        PurchaseOrder order3 = new PurchaseOrder();
-        order.createPurchaseOrder("000001","1",100,"12/20/2022");
-        order.addPurchaseItems(items.get(0).getItemID());
-        PurchaseOrders.add(order);
-        order2.addPurchaseItems(items.get(1).getItemID());
-        order2.createPurchaseOrder("000002","0",1025,"12/21/2022");
-        PurchaseOrders.add(order2);
-        order3.addPurchaseItems(items.get(2).getItemID());
-        order3.createPurchaseOrder("000003","2",103,"12/15/2022");
-        PurchaseOrders.add(order3);
-         */
         UserAccountArray.testAccount();
-        JFrame PurchaseOrderMenu = new PurchaseOrderMenuGUI("Purchase Order");
-        PurchaseOrderMenu.setVisible(true);
-
 
         //Enter fake values for customer to work with and fake customer orders
         try {
@@ -88,8 +71,6 @@ public class Main
         //new AccountantView();
         //UserAccountArray.testAccount();
         new LoginMenu();
-
-
 
     }
 }
